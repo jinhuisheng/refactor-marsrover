882fac934645b9cbf405539eb63758c48ae04320
金慧生 <jinhuisheng@wowoshijie.com>
2020-04-26 19:18:02 +0800

    initial

--- src/main/java/com/example/demo/MarsRover.java
+++ src/main/java/com/example/demo/MarsRover.java
@@ -5,4 +5,39 @@ package com.example.demo;
  * @date 2020/4/26.
  */
 public class MarsRover {
+    private final Integer x;
+    private final Integer y;
+    private String direction;
+
+    public MarsRover(Integer x, Integer y, String direction) {
+
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void send(String command) {
+        if (this.direction.equals("S")) {
+            this.direction = "E";
+        } else if (this.direction.equals("E")) {
+            this.direction = "N";
+        } else if (this.direction.equals("N")) {
+            this.direction = "W";
+        } else if (this.direction.equals("W")) {
+            this.direction = "S";
+        }
+    }
+
 }
--- src/test/java/com/example/demo/MarsRoverTest.java
+++ src/test/java/com/example/demo/MarsRoverTest.java
@@ -1,8 +1,66 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @author huisheng.jin
  * @date 2020/4/26.
  */
 public class MarsRoverTest {
+//    @Test
+//    void demo() {
+//        MarsRover marsRover = new MarsRover(0, 0, "N");
+//        marsRover.send("LL");
+//        assertThat(marsRover.getX()).isEqualTo(0);
+//        assertThat(marsRover.getY()).isEqualTo(0);
+//        assertThat(marsRover.getDirection()).isEqualTo("S");
+//    }
+
+
+    @Test
+    void init_marsRover() {
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        assertThat(marsRover.getX()).isEqualTo(0);
+        assertThat(marsRover.getY()).isEqualTo(0);
+        assertThat(marsRover.getDirection()).isEqualTo("N");
+    }
+
+    @Test
+    void direction_change_from_N_to_W_when_turn_left() {
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        marsRover.send("L");
+        assertThat(marsRover.getX()).isEqualTo(0);
+        assertThat(marsRover.getY()).isEqualTo(0);
+        assertThat(marsRover.getDirection()).isEqualTo("W");
+    }
+
+    @Test
+    void direction_change_from_S_to_E_when_turn_left() {
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+        marsRover.send("L");
+        assertThat(marsRover.getX()).isEqualTo(0);
+        assertThat(marsRover.getY()).isEqualTo(0);
+        assertThat(marsRover.getDirection()).isEqualTo("E");
+    }
+
+    @Test
+    void direction_change_from_E_to_N_when_turn_left() {
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        marsRover.send("L");
+        assertThat(marsRover.getX()).isEqualTo(0);
+        assertThat(marsRover.getY()).isEqualTo(0);
+        assertThat(marsRover.getDirection()).isEqualTo("N");
+    }
+
+    @Test
+    void direction_change_from_W_to_S_when_turn_left() {
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        marsRover.send("L");
+        assertThat(marsRover.getX()).isEqualTo(0);
+        assertThat(marsRover.getY()).isEqualTo(0);
+        assertThat(marsRover.getDirection()).isEqualTo("S");
+    }
+
 }
